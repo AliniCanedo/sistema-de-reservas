9236122b09ab30a27a200745f6317eff4f2acfb1
alini <alini.canedo@gmail.com>
2020-11-28 02:04:33 -0300

    adicionando todos os campos do formulario de cadastro e persistindo no banco

--- db.js
+++ db.js
@@ -1,29 +1,38 @@
 const bcrypt = require('bcryptjs');
 
-function createUser(username, password, email, profile, callback){
+function createUser(username, password, email, rg, cpf, cep, logradouro,
+    numero_end, complemento, bairro, estado,
+    numero_cartao, data_cartao, cvv, horario_reserva, numero_de_pessoas,
+    profile, callback) {
     const cryptoPassword = bcrypt.hashSync(password, 10);
-    global.db.collection("users").insertOne({username, password: cryptoPassword, email, profile}, callback);
+    global.db.collection("users").insertOne({
+        username, password: cryptoPassword, email, rg,
+        cpf, cep, logradouro,
+        numero_end, complemento, bairro, estado,
+        numero_cartao, data_cartao, cvv, horario_reserva, numero_de_pessoas,
+        profile
+    }, callback);
 }
 
-function resetPassword(email, callback){
+function resetPassword(email, callback) {
     const utils = require('./utils');
     const newPassword = utils.generatePassword();
     const cryptoPassword = bcrypt.hashSync(newPassword, 10);
-    global.db.collection("users").updateOne({email: email}, { $set: {password: cryptoPassword }}, (err, res) => {
+    global.db.collection("users").updateOne({ email: email }, { $set: { password: cryptoPassword } }, (err, res) => {
         callback(err, res, newPassword);
     });
 }
 
-function countAll(callback){
+function countAll(callback) {
     global.db.collection("users").countDocuments(callback);
 }
 
 const TAMANHO_PAGINA = 5;
-function findAllUsers(pagina, callback){
-    const totalSkip = (pagina-1) * TAMANHO_PAGINA;
+function findAllUsers(pagina, callback) {
+    const totalSkip = (pagina - 1) * TAMANHO_PAGINA;
     global.db.collection("users").find().skip(totalSkip).limit(TAMANHO_PAGINA).toArray(callback);
 }
 
-module.exports = { 
-    createUser, resetPassword, findAllUsers, TAMANHO_PAGINA, countAll 
+module.exports = {
+    createUser, resetPassword, findAllUsers, TAMANHO_PAGINA, countAll
 }
